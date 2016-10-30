9a5eb261616b949a571831230279e46dd80f9daa
Chris Snow <chsnow123@gmail.com>
2016-10-30 17:08:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name='cf_utils',
+  version='0.1',
+  author='Chris Snow',
+  author_email='chsnow123@gmail.com',
+  url='https://github.com/snowch/cf_utils/',
+  packages=['.'],
+)
